14b198d7212626ea9c8e0384d4361adc9667c357
Krzysztof Denicki <denickikrzysztof@gmail.com>
2024-09-07 22:49:10 +0200

    Dodaj pliki projektów.

---
+++ KinoProjekt/ExitButton.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KinoProjekt
+{
+    public class ExitButton : ListBoxItem
+    {
+        static ExitButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ExitButton), new FrameworkPropertyMetadata(typeof(ExitButton)));
+        }
+        public Geometry Icon
+        {
+            get { return (Geometry)GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
+        public static readonly DependencyProperty IconProperty = DependencyProperty.Register("Icon", typeof(Geometry), typeof(ExitButton), new PropertyMetadata(null));
+    }
+}
---
+++ KinoProjekt/Window1.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace KinoProjekt
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+            navframe.Navigate(Home.NavLink);
+            Home.IsSelected = true;
+        }
+
+        private void NavBarSelect(object sender, SelectionChangedEventArgs e)
+        {
+            var selected = sidebar.SelectedItem as NavButton;
+
+            if (selected.Name == "Shutdown")
+            {
+                Application.Current.Shutdown();
+            }
+            else
+            {
+                navframe.Navigate(selected.NavLink);
+            }
+
+
+        }
+    }
+}
